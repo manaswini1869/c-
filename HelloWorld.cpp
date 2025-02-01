8e5b381ed8bfb4f17648884edfa233254a17a1dd
Manaswini Gogineni <mgogineni@wisc.edu>
2025-02-01 11:54:49 -0600

    varaibles, cout

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int file_size=0, counter=0;
+    double sales = 9.99;
+    int a=1, b=9;
+    cout << a << " " << b << endl;
+    int tmp = a;
+    a=b;
+    b = tmp;
+    cout << a << " "<< b << endl;
+    return 0;
+}
---
+++ helloworl1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+namespace first{
+    int m = 1;
+};
+
+namespace second{
+    int m = 2;
+}
+
+int main() {
+    // This is a comment
+    /*
+    This is a
+    multi-line comment
+    */
+    std::cout << "I like Pizza!" << "\n"; // standard character output
+    std::cout << "Its really good" << std::endl; // endl end of line; end line will flush the output buffer
+
+    // declaring a variable
+    int x; // declaration
+    x = 5; // assignment
+    int y = 6;
+    int sum = x + y;
+    std::cout << x << y << "\n";
+    std::cout << sum;
+
+    // double
+    double d = 3.14;
+    double price = 10.99;
+
+    // char
+    char c = 'A'; // char can store only a single character
+    char initial = 'C';
+    char currency = '$';
+
+    // boolean
+    bool isTrue = true;
+    bool isFalse = false;
+
+    // string (string is an object that represents a sequence of text)
+    std::string name = "Manaswini";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::cout << "\n" << name;
+    std::cout << "\n" << "Hello " << name;
+
+    //const keyword tells the compiler that a variable value is constant
+    // tells the compiler to prevent anything from modifying it (read-only)
+    const double PIE = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PIE * radius;
+    const int LIGHT_SPEED = 299792458;
+    std::cout << "\n" << circumference << "cm";
+
+    // Namespace = provides a solution for preventing name conflicts ni large projects. Each entity needs a
+    // unique name. A namespace allows for identically name entities as long as the namespaces are different
+    using namespace first;
+    // int m = 0;
+    std::cout << "\n" << "Printing x " <<m; // if not declared explicitly we will be using the local version of the variable
+
+    std:: cout << m; // the double colon here is the scope resolution
+
+    return 0;
+}
